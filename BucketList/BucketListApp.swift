c7ba59b03dddebe39ca89b161c83a2cddd08fe5e
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-29 22:59:25 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Khumbongmayum Tonny on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
