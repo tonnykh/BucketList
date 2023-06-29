9ae76a85a06f93f926ec0e924f2a2cbc1c07befd
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-29 23:23:17 +0530

    Adding user location to the map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,54 @@
 //  Created by Khumbongmayum Tonny on 29/06/23.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Codable, Identifiable, Equatable {
+    var id = UUID()
+    let name: String
+    let description: String
+    let latitude: Double
+    let longitude: Double
+}
+
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    @State private var locations = [Location]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapMarker  (coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+            }
+            .ignoresSafeArea()
+            
+            Circle()
+                .foregroundColor(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        // create a new location
+                        let newLocation = Location(name: "", description: "New Location", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.7))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
         }
-        .padding()
     }
 }
 
